3c1795d03410228bd8f4e59679f0620028b1c3ae
Glenn Vona <glennvonaCB5@MacBook-Air-10.local>
2019-09-23 10:48:18 -0400

    removed import comment

--- models/connection.js
+++ models/connection.js
@@ -4,12 +4,6 @@ const mongoose = require('mongoose');
 const connectionString = process.env.MONGODB_URI || "mongodb://localhost/keeper";
 
 
-/* Step 2
- *
- * Open up a connection to the mongo database.
- *
- * NOTE: newUrlParser diables a deprecation warning
- */
 mongoose.connect(connectionString, { useNewUrlParser: true})
   .then(() => {
     console.log("connected to mongo at: " + connectionString);
